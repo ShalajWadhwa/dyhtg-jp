228d6d975d533de3dccc6acaa3df7b25f5f9c334
Shalaj Wadhwa <48180546+ShalajWadhwa@users.noreply.github.com>
2022-02-13 00:07:47 +0000

    Tweaked employee functions

--- backend/employee.py
+++ backend/employee.py
@@ -1,51 +1,33 @@
 import json
 from geopy.geocoders import Nominatim
+from coords_api_func import get_coords
 
-user_User = {}
-totalEmployees = 50
+# with open('../database/db.json', 'r+') as openfile:
 
-with open(r'C:\Users\hello\Documents\GitHub\dhytg-jp\db.json', 'r+') as openfile:
+#     json_object = json.load(openfile)
 
-    json_object = json.load(openfile)
+# user_Library = json_object
 
-user_Library = json_object
-
-
-def user_GenerateEmployeeNumber():
+# totalEmployees = 50
+# def user_GenerateEmployeeNumber():
     
-    global totalEmployees
-    totalEmployees += 1
-    temp_user_EmployeeNumber = 0000 + totalEmployees
+#     global totalEmployees
+#     totalEmployees += 1
+#     temp_user_EmployeeNumber = 0000 + totalEmployees
 
-    return temp_user_EmployeeNumber
+#     return temp_user_EmployeeNumber
 
-def get_coords(postCode):
-    locator = Nominatim(user_agent="myGeocoder")
-    location = locator.geocode(postCode)
-    return location.latitude, location.longitude
-
-
-def user_Create( firstname, lastname, email, phone_number, city, post_code, weather, news, service ):
-
-    user_EmployeeNumber = user_GenerateEmployeeNumber()
+def user_Create(user_EmployeeNumber, firstname, lastname, email, phone_number, city, post_code, weather, news, service):
+#   user_EmployeeNumber = user_GenerateEmployeeNumber()
     user_NewUser = { user_EmployeeNumber :  {} }
-
     user_NewUser[user_EmployeeNumber]["first_name"] = firstname
-
     user_NewUser[user_EmployeeNumber]["last_name"] = lastname
-
     user_NewUser[user_EmployeeNumber]["email"] = email
-
     user_NewUser[user_EmployeeNumber]["phone_number"] = phone_number
-
     user_NewUser[user_EmployeeNumber]["city"] = city
-
     user_NewUser[user_EmployeeNumber]["post_code"] = post_code
-
     user_NewUser[user_EmployeeNumber]["weather"] = weather
-
     user_NewUser[user_EmployeeNumber]["news"] = news
-
     user_NewUser[user_EmployeeNumber]["service"] = service
 
     #location API
@@ -54,37 +36,41 @@ def user_Create( firstname, lastname, email, phone_number, city, post_code, weat
     user_NewUser[user_EmployeeNumber]["lat"] = lat
     user_NewUser[user_EmployeeNumber]["long"] = long
 
-    with open(r'C:\Users\hello\Documents\GitHub\dhytg-jp\db.json', 'r+') as openfile:
+    with open('database/db.json', 'r+') as openfile:
         data = json.load(openfile)
-        data.update( user_NewUser )
+        data.update(user_NewUser)
         openfile.seek(0)
         json.dump(data, openfile)
 
-def user_Delete( user_Library, name ):
-   
-    with open(r'C:\Users\hello\Documents\GitHub\dhytg-jp\db.json', 'r') as openfile:
+#####
+
+def user_Delete(employee_number):
+    with open('database/db.json', 'r') as openfile:
         user_Library = json.load(openfile)
 
-    del user_Library[user_Search( user_Library, name)[0]]
-
-    with open(r'C:\Users\hello\Documents\GitHub\dhytg-jp\db.json', 'w') as openfile:
-        user_Library = json.dump(user_Library, openfile)
-
-
-
-def user_Information( input_UserLibrary ):
-    for key, value in input_UserLibrary.items():
-        print(key, end=": ")
-        if type(value) is str or float:
-            print(value, end=" ")
-        if type(value) == None:
-            pass
-        else:
-            value
+    if employee_number in user_Library.keys():
+        del user_Library[employee_number]
+        with open('database/db.json', 'w') as openfile:
+            user_Library = json.dump(user_Library, openfile)
+        return "Employee Removed"
+    else:
+        return "Employee Not Found"
 
 
-def user_Search( user_Library, search_input):
+# def user_Information(input_UserLibrary):
+#     for key, value in input_UserLibrary.items():
+#         print(key, end=": ")
+#         if type(value) is str or float:
+#             print(value, end=" ")
+#         if type(value) == None:
+#             pass
+#         else:
+#             value
 
+
+def user_Search(search_input):
+    with open('database/db.json', 'r') as openfile:
+        user_Library = json.load(openfile)
     try:
         for temp_UserNumber in user_Library:
             if search_input in user_Library[temp_UserNumber].values():
@@ -95,12 +81,27 @@ def user_Search( user_Library, search_input):
     except:
             print("Error with Search")
 
+
+######
+
+
+
 # user_Search( user_Library, "Hessel" )
-user_Create("firstname", "lastname", "email", "phone_number", "city", "DD5 1DZ", "weather", "news", "service" )
+
+################################## Data Fill - Shalaj
+# emp_number = 0
+# for emp_number in range(20):
+#     user_Create(emp_number+1, "a", "b", "c@mail.com", "1111111111", "Glasgow", "G12 8QQ", None, None, None)
+##################################
+
+# print(user_Delete("101"))
 
 # weather api function (loop)
 # for all employees get weather api using employees lon/lat
-print(user_Search( user_Library, "Daniel"))
+
+#################################################################print(user_Search( user_Library, "Daniel")) # Uncomment
+
+
 # user_Delete( user_Library, "firstname" )
 # print(user_Library)
 
